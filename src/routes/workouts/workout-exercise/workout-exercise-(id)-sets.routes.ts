1582ac8376fb5f04c0b680c703c9233ddb1412cd
michal_src <c_ukiereczek.p@onet.pl>
2025-04-27 21:43:33 +0200

    zmiany 27.04.2025

--- src/routes/workouts/workout-exercise/workout-exercise-(id)-sets.routes.ts
+++ src/routes/workouts/workout-exercise/workout-exercise-(id)-sets.routes.ts
@@ -1,36 +1,56 @@
-import { workout_sets } from '@db/schema';
-import dbConnection from '@src/db/connection';
-import { Hono } from 'hono';
+import { workout_sets } from "@db/schema"
+import dbConnection from "@src/db/connection"
+import { asc, eq } from "drizzle-orm"
+import { Hono } from "hono"
 
-const app = new Hono();
+const app = new Hono()
 
-app.post('/exercises/:id/sets', async (c) => {
-	const request_params = await c.req.param();
-	const request_body = await c.req.json();
+app.get("/exercise/:id/sets", async (c) => {
+  const request_params = await c.req.param()
 
-	if (Number.isNaN(request_params?.id)) {
-		c.status(400);
-		return c.json(null);
-	}
+  if (Number.isNaN(request_params?.id)) {
+    c.status(400)
+    return c.json(null)
+  }
 
-	const _params_workout_exercise_id = Number(request_params?.id);
-	const _body_workout_set_weight = request_body?.weight ?? 0;
-	const _body_workout_set_count = request_body?.count ?? 0;
+  const _params_workout_exercise_id = Number(request_params?.id)
 
-	const db = await dbConnection(c);
-	const _result_insert = await db
-		.insert(workout_sets)
-		.values({
-			workout_exercise_id: _params_workout_exercise_id,
-			count: _body_workout_set_count,
-			weight: _body_workout_set_weight,
-		})
-		.returning();
+  const db = await dbConnection(c)
+  const _result_workout_exercise_sets = await db.select().from(workout_sets).orderBy(asc(workout_sets.order)).where(eq(workout_sets.workout_exercise_id, _params_workout_exercise_id))
 
-	return c.json({
-		Items: _result_insert,
-		TotalCount: _result_insert?.length || 0,
-	});
-});
+  return c.json({
+    Items: _result_workout_exercise_sets,
+    TotalCount: _result_workout_exercise_sets?.length || 0,
+  })
+})
 
-export default app;
+app.post("/exercises/:id/sets", async (c) => {
+  const request_params = await c.req.param()
+  const request_body = await c.req.json()
+
+  if (Number.isNaN(request_params?.id)) {
+    c.status(400)
+    return c.json(null)
+  }
+
+  const _params_workout_exercise_id = Number(request_params?.id)
+  const _body_workout_set_weight = request_body?.weight ?? 0
+  const _body_workout_set_count = request_body?.count ?? 0
+
+  const db = await dbConnection(c)
+  const _result_insert = await db
+    .insert(workout_sets)
+    .values({
+      workout_exercise_id: _params_workout_exercise_id,
+      count: _body_workout_set_count,
+      weight: _body_workout_set_weight,
+    })
+    .returning()
+
+  return c.json({
+    Items: _result_insert,
+    TotalCount: _result_insert?.length || 0,
+  })
+})
+
+export default app
--- src/routes/workouts/workout-exercise/workout-exercise-(id).routes.ts
+++ src/routes/workouts/workout-exercise/workout-exercise-(id).routes.ts
@@ -1,77 +1,54 @@
-import { workout_exercises, workout_sets } from '@db/schema';
-import dbConnection from '@src/db/connection';
-import { asc, eq } from 'drizzle-orm';
-import { Hono } from 'hono';
+import { workout_exercises, workout_sets } from "@db/schema"
+import dbConnection from "@src/db/connection"
+import { eq } from "drizzle-orm"
+import { Hono } from "hono"
 
-const app = new Hono();
+const app = new Hono()
 
-app.get('/exercise/:id', async (c) => {
-	const request_params = await c.req.param();
+app.post("/exercise/:id", async (c) => {
+  const request_params = await c.req.param()
+  const request_body = await c.req.json()
 
-	if (Number.isNaN(request_params?.id)) {
-		c.status(400);
-		return c.json(null);
-	}
+  if (Number.isNaN(request_params?.id)) {
+    c.status(400)
+    return c.json(null)
+  }
 
-	const _params_workout_exercise_id = Number(request_params?.id);
+  const _params_workout_exercise_id = Number(request_params?.id)
+  const _body_workout_exercise_sets = request_body?.exerciseSets ?? []
 
-	const db = await dbConnection(c);
-	const _result_workout_exercise_sets = await db
-		.select()
-		.from(workout_sets)
-		.orderBy(asc(workout_sets.order))
-		.where(eq(workout_sets.workout_exercise_id, _params_workout_exercise_id));
+  const _workout_exercise_sets = _body_workout_exercise_sets.map((set) => ({
+    workout_exercise_id: _params_workout_exercise_id,
+    count: set.count,
+    weight: set.weight,
+  }))
 
-	return c.json({
-		Items: _result_workout_exercise_sets,
-		TotalCount: _result_workout_exercise_sets?.length || 0,
-	});
-});
+  const db = await dbConnection(c)
+  const _result_insert = await db.insert(workout_sets).values(_workout_exercise_sets).returning()
 
-app.post('/exercise/:id', async (c) => {
-	const request_params = await c.req.param();
-	const request_body = await c.req.json();
+  return c.json({
+    Items: _result_insert,
+    TotalCount: _result_insert?.length || 0,
+  })
+})
 
-	if (Number.isNaN(request_params?.id)) {
-		c.status(400);
-		return c.json(null);
-	}
+app.delete("/exercise/:id", async (c) => {
+  const request_params = await c.req.param()
 
-	const _params_workout_exercise_id = Number(request_params?.id);
-	const _body_workout_exercise_sets = request_body?.exerciseSets ?? [];
+  if (Number.isNaN(request_params?.id)) {
+    c.status(400)
+    return c.json(null)
+  }
 
-	const _workout_exercise_sets = _body_workout_exercise_sets.map((set) => ({
-		workout_exercise_id: _params_workout_exercise_id,
-		count: set.count,
-		weight: set.weight,
-	}));
+  const _params_workout_exercise_id = Number(request_params?.id)
 
-	const db = await dbConnection(c);
-	const _result_insert = await db.insert(workout_sets).values(_workout_exercise_sets).returning();
+  const db = await dbConnection(c)
+  await db.delete(workout_exercises).where(eq(workout_exercises.id, _params_workout_exercise_id))
+  await db.delete(workout_sets).where(eq(workout_sets.workout_exercise_id, _params_workout_exercise_id))
 
-	return c.json({
-		Items: _result_insert,
-		TotalCount: _result_insert?.length || 0,
-	});
-});
+  return c.json({
+    status: true,
+  })
+})
 
-app.delete('/exercise/:id', async (c) => {
-	const request_params = await c.req.param();
-
-	if (Number.isNaN(request_params?.id)) {
-		c.status(400);
-		return c.json(null);
-	}
-
-	const _params_workout_exercise_id = Number(request_params?.id);
-
-	const db = await dbConnection(c);
-	await db.delete(workout_exercises).where(eq(workout_exercises.id, _params_workout_exercise_id));
-	await db.delete(workout_sets).where(eq(workout_sets.workout_exercise_id, _params_workout_exercise_id));
-
-	return c.json({
-		status: true,
-	});
-});
-
-export default app;
+export default app
